56eddaaa110af690d893182f3a682b222df32ffd
Florentak <florentkossiayidedji@gmail.com>
2024-12-02 21:46:03 +0100

    correction mineure: mise à jour du contenu de Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import Footer from './components/footer';
+import Header from "../components/Header/Header";
+import { NavbarItem } from "../components/Header/Navbar/Navbar";
+import Articles from "../components/Articles/Articles";
+import Pagination from "../components/Pagination/Pagination";
+import Footer from "../components/Footer/Footer";
+
+const navElements: NavbarItem = {
+    navbarLogo: 'https://websitedemos.net/blog-02/wp-content/uploads/sites/132/2017/11/Interior-logo4@2x-free-img.png',
+    navbarLinks: [
+        'About Us',
+        'Living Room',
+        'Kitchen',
+        'Outdoors',
+        'Contact'
+    ]
+}
+
+
+
 
 
 function Home()
 {
+   return (
     <div>
-       <Footer/>
+        <Header navElements={navElements} />
+        <div className="section-container">
+            <Articles />
+            <Pagination />
+        </div>
+        <Footer />
     </div>
+   );
 }
 
 export default Home;
